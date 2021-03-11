553f1abe83eee79efb7295fc47d53ce1831c0adf
Vladimir Martynenko <Vladimir@Martynenko.me>
2021-03-10 20:14:25 -0500

    Removed files needed for the previous homework

--- build.bat
+++
@@ -1,3 +0,0 @@
-javac -cp build;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar src/main/java/*.java src/test/java/*.java
-java -cp build;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java -cp build RunEdgeConvert
