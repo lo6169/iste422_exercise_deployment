671bad37a63678d32acc4786ae4de03ecfa2a88b
lo6169 <lo6169@rit.edu>
2021-03-13 14:15:34 -0500

    Began working on EdgeFieldTest.java. Started working on the test for getters and setters.

--- src/test/java/EdgeFieldTest.java
+++ src/test/java/EdgeFieldTest.java
@@ -3,11 +3,90 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class EdgeFieldTest {
+public class EdgeFieldTest
+{
   EdgeField testObj;
 
   @Before
-  public void setUp() throws Exception {
-//    testObj = new EdgeField();
+  public void setUp() throws Exception
+  {
+    testObj = new EdgeField("String");
+    System.out.println("Testing EdgeField...");
+
+    System.out.println("Test 1: Ensure getters are working...");
+    testGetTableID();
+    testGetTableBound();
+
+    System.out.println("Test 2: Ensure setters are working...");
+
+
+    System.out.println("Test 3: Ensure errors are thrown when needed...");
   }
+
+  // Test some of the getters
+
+  /*
+   The tableID should automatically be set to 0
+   Check if it is. If it isn't, throw an error.
+   */
+  public boolean testGetTableID()
+  {
+    if (testObj.getTableID() == 0)
+    {
+      System.out.println("Test 1.1 passed - getTableID.");
+      return true;
+    }
+    else
+    {
+      System.out.println("Test 1.1 failed - getTableID.");
+      return false;
+    }
+  }
+
+  /*
+   The tableBound should automatically be set to 0
+   Check if it is. If it isn't, throw an error.
+   */
+  public boolean testGetTableBound()
+  {
+    if (testObj.getTableBound() == 0)
+    {
+      System.out.println("Test 1.2 passed - getTableBound.");
+      return true;
+    }
+    else
+    {
+      System.out.println("Test 1.2 failed - getTableBound.");
+      return false;
+    }
+  }
+
+  // Test some of the setters
+
+  /*
+  If the getters passed, then we need to check
+  to ensure that the setters work.
+  Set the table ID to a random integer, and
+  check to see that it returns the same one.
+   */
+  public boolean testSetTableID()
+  {
+    testObj.setTableID(27);
+    if (testObj.getTableID() == 27)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  // Test the toString function
+  public boolean testToString()
+  {
+    return false;
+  }
+
+
 }
