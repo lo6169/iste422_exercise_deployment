bd25e7673a6cb57ea2b1e5882ab4c2cb65226369
lo6169 <lo6169@rit.edu>
2021-03-28 15:37:04 -0400

    Hopefully fixed the broken build by updating build.gradle and EdgeField.java

--- src/main/java/EdgeField.java
+++ src/main/java/EdgeField.java
@@ -8,7 +8,7 @@ public class EdgeField {
    private boolean disallowNull, isPrimaryKey;
    private static String[] strDataType = {"Varchar", "Boolean", "Integer", "Double"};
    public static final int VARCHAR_DEFAULT_LENGTH = 1;
-   public static Logger logger = LogManager.getLogger(CreateDDLMySQL.class.getName());
+   public static Logger logger = LogManager.getLogger(EdgeField.class.getName());
    
    public EdgeField(String inputString) {
       logger.debug("EdgeField constructor called with the input of " + inputString);
