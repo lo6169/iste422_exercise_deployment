80d208c9a09b394eed08aafa902489dcdca31f20
lo6169 <lo6169@rit.edu>
2021-03-28 15:28:48 -0400

    Created the logs using logger for EdgeField.java.

--- src/main/java/EdgeField.java
+++ src/main/java/EdgeField.java
@@ -1,4 +1,6 @@
 import java.util.StringTokenizer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EdgeField {
    private int numFigure, tableID, tableBound, fieldBound, dataType, varcharValue;
@@ -6,8 +8,10 @@ public class EdgeField {
    private boolean disallowNull, isPrimaryKey;
    private static String[] strDataType = {"Varchar", "Boolean", "Integer", "Double"};
    public static final int VARCHAR_DEFAULT_LENGTH = 1;
+   public static Logger logger = LogManager.getLogger(CreateDDLMySQL.class.getName());
    
    public EdgeField(String inputString) {
+      logger.debug("EdgeField constructor called with the input of " + inputString);
       StringTokenizer st = new StringTokenizer(inputString, EdgeConvertFileParser.DELIM);
       numFigure = Integer.parseInt(st.nextToken());
       name = st.nextToken();
@@ -22,85 +26,113 @@ public class EdgeField {
    }
    
    public int getNumFigure() {
+      logger.debug("getNumFigure has been called.");
       return numFigure;
    }
    
    public String getName() {
+      logger.debug("getName has been called.");
       return name;
    }
    
    public int getTableID() {
+      logger.debug("getTableID has been called.");
       return tableID;
    }
    
    public void setTableID(int value) {
+      logger.debug("setTableID has been called with a value of " + value);
       tableID = value;
    }
    
    public int getTableBound() {
+      logger.debug("getTableBound has been called.");
       return tableBound;
    }
    
    public void setTableBound(int value) {
+      logger.debug("setTableBound has been called with a value of " + value);
       tableBound = value;
    }
 
    public int getFieldBound() {
+      logger.debug("getFieldBound has been called.");
       return fieldBound;
    }
    
    public void setFieldBound(int value) {
+      logger.debug("setFieldBound has been called with a value of " + value);
       fieldBound = value;
    }
 
    public boolean getDisallowNull() {
+      logger.debug("getDisallowNull has been called.");
       return disallowNull;
    }
    
    public void setDisallowNull(boolean value) {
+      logger.debug("setDisallowNull has been called with a value of " + value);
       disallowNull = value;
    }
    
    public boolean getIsPrimaryKey() {
+      logger.debug("getIsPrimaryKey has been called.");
       return isPrimaryKey;
    }
    
    public void setIsPrimaryKey(boolean value) {
+      logger.debug("setIsPrimaryKey has been called with a value of " + value);
       isPrimaryKey = value;
    }
    
    public String getDefaultValue() {
+      logger.debug("getDefaultValue has been called.");
       return defaultValue;
    }
    
    public void setDefaultValue(String value) {
+      logger.debug("setDefaultValue has been called with a value of " + value);
       defaultValue = value;
    }
    
    public int getVarcharValue() {
+      logger.debug("getVarcharValue has been called.");
       return varcharValue;
    }
    
    public void setVarcharValue(int value) {
+      logger.debug("setVarcharValue has been called with a value of " + value);
       if (value > 0) {
          varcharValue = value;
       }
+      else
+      {
+         logger.warn("setVarcharValue called with a value less than or equal to 0.");
+      }
    }
    public int getDataType() {
+      logger.debug("getDataType has been called.");
       return dataType;
    }
    
    public void setDataType(int value) {
+      logger.debug("setDataType has been called with a value of " + value);
       if (value >= 0 && value < strDataType.length) {
          dataType = value;
+      }
+      else
+      {
+         logger.warn("SetDataType called with a value less than 0, or greater than the length of the string.");
       }
    }
    
    public static String[] getStrDataType() {
+      logger.debug("getStrDataType has been called.");
       return strDataType;
    }
    
    public String toString() {
+      logger.info("toString has been called.");
       return numFigure + EdgeConvertFileParser.DELIM +
       name + EdgeConvertFileParser.DELIM +
       tableID + EdgeConvertFileParser.DELIM +
